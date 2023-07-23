02ee965508f40963365807c51baa09b9b380986b
Hououin Kyouma <inwazzin@gmail.com>
2023-07-23 16:30:55 +0200

    move creation on canvas mount

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,11 @@
-import { create } from '@zd/engine';
 import { Footer, Header, Content, SideBar } from '@layout/layout.js';
 import s from './App.module.scss';
 
-export const App = () => {
-  console.log(create);
-
-  return (
-    <div className={s.app}>
-      <SideBar />
-      <Header />
-      <Content />
-      <Footer />
-    </div>
-  );
-};
+export const App = () => (
+  <div className={s.app}>
+    <SideBar />
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+);
