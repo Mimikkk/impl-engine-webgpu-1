bacdb944b03eab24b45bbad6e13e399352d6f44e
Hououin Kyouma <inwazzin@gmail.com>
2023-08-29 23:59:47 +0200

    cone

---
+++ packages/client/src/renderers/webgpu/core/geometries/ConeGeometry.ts
@@ -0,0 +1,47 @@
+import { CylinderGeometry } from './CylinderGeometry.js';
+
+export class ConeGeometry extends CylinderGeometry {
+  constructor(
+    radius: number = 1,
+    height: number = 1,
+    radialSegments: number = 32,
+    heightSegments: number = 1,
+    openEnded: boolean = false,
+    thetaStart: number = 0,
+    thetaLength: number = Math.PI * 2,
+  ) {
+    super(0, radius, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength);
+
+    this.type = 'ConeGeometry';
+
+    this.parameters = {
+      radius: radius,
+      height: height,
+      radialSegments: radialSegments,
+      heightSegments: heightSegments,
+      openEnded: openEnded,
+      thetaStart: thetaStart,
+      thetaLength: thetaLength,
+    };
+  }
+
+  static fromJSON(data: {
+    radius: number;
+    height: number;
+    radialSegments: number;
+    heightSegments: number;
+    openEnded: boolean;
+    thetaStart: number;
+    thetaLength: number;
+  }) {
+    return new ConeGeometry(
+      data.radius,
+      data.height,
+      data.radialSegments,
+      data.heightSegments,
+      data.openEnded,
+      data.thetaStart,
+      data.thetaLength,
+    );
+  }
+}
