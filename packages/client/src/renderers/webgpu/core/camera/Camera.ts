a30c6a7cda43cc5f7794ca373f8b4a69f57d1df2
Hououin Kyouma <inwazzin@gmail.com>
2023-08-31 21:51:35 +0200

    camera typing

--- packages/client/src/renderers/webgpu/core/camera/Camera.ts
+++ packages/client/src/renderers/webgpu/core/camera/Camera.ts
@@ -6,17 +6,13 @@ import { Object3D } from '../Object3D.js';
 export class Camera extends Object3D {
   isCamera: boolean = true;
   type: string = 'Camera';
-  matrixWorldInverse: Matrix4;
-  projectionMatrix: Matrix4;
-  coordinateSystem: number;
-  projectionMatrixInverse: Matrix4;
+  matrixWorldInverse: Matrix4 = new Matrix4();
+  projectionMatrix: Matrix4 = new Matrix4();
+  coordinateSystem: number = WebGLCoordinateSystem;
+  projectionMatrixInverse: Matrix4 = new Matrix4();
 
   constructor() {
     super();
-    this.matrixWorldInverse = new Matrix4();
-    this.projectionMatrix = new Matrix4();
-    this.projectionMatrixInverse = new Matrix4();
-    this.coordinateSystem = WebGLCoordinateSystem;
   }
 
   copy(source: Camera, recursive?: boolean): Camera {
@@ -32,25 +28,19 @@ export class Camera extends Object3D {
 
   getWorldDirection(target: Vector3) {
     this.updateWorldMatrix(true, false);
-
     const e = this.matrixWorld.elements;
-
     return target.set(-e[8], -e[9], -e[10]).normalize();
   }
 
   updateMatrixWorld(force?: boolean) {
     super.updateMatrixWorld(force);
-
     this.matrixWorldInverse.copy(this.matrixWorld).invert();
   }
 
   updateWorldMatrix(updateParents: boolean, updateChildren: boolean) {
     super.updateWorldMatrix(updateParents, updateChildren);
-
     this.matrixWorldInverse.copy(this.matrixWorld).invert();
   }
 
-  clone(recursive?: boolean): Camera {
-    return new Camera().copy(this, recursive);
-  }
+  clone = (recursive?: boolean): Camera => new Camera().copy(this, recursive);
 }
