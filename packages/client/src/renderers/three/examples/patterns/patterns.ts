b549852807c960735956b03761f33672882a2c32
Hououin Kyouma <inwazzin@gmail.com>
2023-09-29 12:42:15 +0200

    audio processing

--- packages/client/src/renderers/three/examples/patterns/patterns.ts
+++ packages/client/src/renderers/three/examples/patterns/patterns.ts
@@ -1,5 +1,6 @@
 import { PerspectiveCamera } from '../../src/cameras/PerspectiveCamera.js';
 import { WebGPURenderer } from '../../src/WebGPURenderer.js';
+import { Renderer } from '../../../webgpu/createRenderer.js';
 
 export const createResizer = (renderer: WebGPURenderer, camera: PerspectiveCamera) => {
   window.addEventListener('resize', () => {
@@ -8,5 +9,12 @@ export const createResizer = (renderer: WebGPURenderer, camera: PerspectiveCamer
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
 };
-:
-export const containRenderer = () => {};
+
+export const containRenderer = (renderer: Renderer) => {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  container.appendChild(renderer.domElement);
+};
